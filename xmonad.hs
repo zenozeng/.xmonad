558be282637d8046854a7549be4c95451e61b2b5
Zeno Zeng <zenoes@qq.com>
2013-05-09 19:12:51 +0800

    add keybinding for focus

--- xmonad.hs
+++ xmonad.hs
@@ -3,18 +3,20 @@ import XMonad.Actions.CycleWS
 import XMonad.Actions.CycleWindows
 import XMonad.Util.EZConfig        -- append key/mouse bindings
 import XMonad.Layout.IndependentScreens  
+
+import qualified XMonad.StackSet as W
   
 -- 关于GTK的外观，建议使用一个角lxappearance的东西设置一下GTK的主题
 -- 切换显示器只要将鼠标移到另一个显示器就好
 
 myKeys = [ 
-  ("M1-<Tab>"   , cycleRecentWindows [xK_Alt_L] xK_Tab xK_Tab ) -- classic alt-tab behaviour
+  ("M1-<Tab>"   , windows W.swapMaster)
   , ("M-<Return>" , spawn "xterm")
-  , ("M-<Tab>"    , toggleWS                              ) -- toggle last workspace (super-tab)
-  , ("M-n"  , nextWS                                ) -- go to next workspace
-  , ("M-p"  , prevWS                                ) -- go to prev workspace
-  , ("M-S-n", shiftToNext                           ) -- move client to next workspace
-  , ("M-S-p" , shiftToPrev                           ) -- move client to prev workspace
+  , ("M-<Tab>", windows W.focusDown)
+  , ("M-n"  , nextWS) -- go to next workspace
+  , ("M-p"  , prevWS) -- go to prev workspace
+  , ("M-S-n", shiftToNext) -- move client to next workspace
+  , ("M-S-p" , shiftToPrev) -- move client to prev workspace
   , ("M-w", kill)
 
   -- volume control 
@@ -22,7 +24,7 @@ myKeys = [
   , ("M--", spawn "amixer sset Master 10%-")
 
   -- apps
-  , ("M-r"        , spawn "gmrun"                         ) -- app launcher
+  , ("M-r", spawn "gmrun"                         ) -- app launcher
   , ("M-g", spawn "google-chrome")
   , ("M-f", spawn "firefox")
   , ("M-e", spawn "emacsclient -c -a '' --no-wait")  
