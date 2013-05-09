3fa914f348cef6d8f9f2bd2016eeab2be5e76258
Zeno Zeng <zenoes@qq.com>
2013-05-09 14:19:41 +0800

    update xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -2,8 +2,6 @@ import XMonad
 import XMonad.Actions.CycleWS    
 import XMonad.Actions.CycleWindows
 import XMonad.Util.EZConfig        -- append key/mouse bindings
-import XMonad.Layout.NoBorders     -- no borders on solo clients
-
   
 myKeys = [ 
   ("M1-<Tab>"   , cycleRecentWindows [xK_Alt_L] xK_Tab xK_Tab ) -- classic alt-tab behaviour
@@ -13,10 +11,19 @@ myKeys = [
   , ("M-p"  , prevWS                                ) -- go to prev workspace
   , ("M-S-n", shiftToNext                           ) -- move client to next workspace
   , ("M-S-p" , shiftToPrev                           ) -- move client to prev workspace
+  , ("M-w", kill)
+
+  -- volume control 
+  , ("M-=", spawn "amixer sset Master 10%+")
+  , ("M--", spawn "amixer sset Master 10%-")
+
+  -- apps
   , ("M-r"        , spawn "gmrun"                         ) -- app launcher
+  , ("M-g", spawn "google-chrome")
   , ("M-f", spawn "firefox")
-  , ("M-w", kill) -- launch browser
-  , ("M-e"        , spawn "nautilus"                      ) -- launch file manager
+  , ("M-e", spawn "emacsclient -c -a '' --no-wait")  
+  --  , ("M-e"        , spawn "nautilus"                      ) -- launch file manager
+
   , ("M-q"        , spawn "xmonad --restart"              ) -- restart xmonad w/o recompiling
   ]
          
@@ -28,7 +35,7 @@ main = xmonad $defaultConfig
          focusFollowsMouse  = True,
          borderWidth        = 2,
          modMask            = mod4Mask, -- use super
-         normalBorderColor  = "#111111",
-         focusedBorderColor = "#111111"
+         normalBorderColor  = "#333",
+         focusedBorderColor = "#333"
        }
        `additionalKeysP` myKeys
